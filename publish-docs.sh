ea93c08c53ab2960aa5026043be37ada8c1c4b92
Andy Spohn <andy@cfn-stacks.com>
2017-08-21 07:32:51 -0400

    add favicon to pages

--- publish-docs.sh
+++ publish-docs.sh
@@ -6,3 +6,4 @@ command -v aws >/dev/null 2>&1 || { pip install --user awscli; export PATH=$PATH
 
 aws s3 cp index.html s3://cfn-stacks.com/index.html
 aws s3 cp docs/index.html s3://cfn-stacks.com/docs/index.html
+aws s3 sync images/ s3://cfn-stacks.com/images/
